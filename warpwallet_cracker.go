e858b718776243b39d846fcda648337142cbcdfd
mkvtvseries <mkvtvseries@users.noreply.github.com>
2017-12-10 01:25:47 +0530

    Update warpwallet_cracker.go

--- warpwallet_cracker.go
+++ warpwallet_cracker.go
@@ -2,14 +2,14 @@ package main
 
 import (
   pbkdf2 "github.com/ctz/go-fastpbkdf2"
-	"golang.org/x/crypto/scrypt"
+	"github.com/golang/crypto/blob/master/scrypt"
   "unsafe"
 	"crypto/sha256"
 	"fmt"
 	"time"
 	"os"
 	"math/rand"
-	"https://github.com/mkvtvseries/btckeygenie"
+	"github.com/mkvtvseries/btckeygenie/blob/master/btckey/"
 )
 
 const wordSize = int(unsafe.Sizeof(uintptr(0)))
